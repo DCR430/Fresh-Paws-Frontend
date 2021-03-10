ea17f81946522d7e78e2984d71a97529199eb0ed
DCR430 <daniel.c.reyes30@gmail.com>
2021-03-09 19:06:43 -0500

    map

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,15 +1,3 @@
-// import React, { Component } from 'react'
-
-// export default class Profile extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 Profile
-                
-//             </div>
-//         )
-//     }
-// }
 
 ////////////////////////////////
 
